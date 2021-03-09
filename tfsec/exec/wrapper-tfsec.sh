a34b4a32fc0ffd4cad8c0c9523c7dc99667e5362
Filip Tepper <filip@tepper.pl>
2021-03-09 15:03:26 +0100

    initial wrapper

---
+++ tfsec/exec/wrapper-tfsec.sh
@@ -0,0 +1,7 @@
+#!/bin/bash +e
+
+tfsec --format=checkstyle -e AWS045 | reviewdog -f=checkstyle -reporter=github-pr-review
+
+tfsec_return="${PIPESTATUS[0]}" reviewdog_return="${PIPESTATUS[1]}" exit_code=$?
+
+exit $exit_code
