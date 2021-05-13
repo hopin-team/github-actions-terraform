89d11fd052c2a460db479c1afeca2fe1961b3482
Filip Tepper <filip@tepper.pl>
2021-05-13 12:29:55 +0200

    enable debug data

--- tflint/exec/wrapper-tflint.sh
+++ tflint/exec/wrapper-tflint.sh
@@ -13,9 +13,13 @@ fi
 for _FILE in $_FILES
 do
   if [ "${_FILE: -3}" == ".tf" ]; then
+    echo "Checking file: $_FILE"
+
     if ! tflint -f checkstyle "$_FILE" -c "$_TFLINT_HCL_PATH" > _TFLINT; then
       _EXIT_CODE="1"
     fi
+
+    cat _TFLINT
 
     cat _TFLINT | reviewdog -f=checkstyle -name="tflint" -reporter=github-pr-review
   fi
