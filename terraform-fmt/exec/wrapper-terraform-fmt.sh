7a369ebb7c6bfac845c24495f9cf116ce42ab2ff
cnewkirk <40219357+cnewkirk@users.noreply.github.com>
2021-11-19 11:08:15 -0800

    oh right, that's .. a file .. not a variable

--- terraform-fmt/exec/wrapper-terraform-fmt.sh
+++ terraform-fmt/exec/wrapper-terraform-fmt.sh
@@ -9,7 +9,7 @@ while IFS= read -r FILE; do
 done < _TERRAFORM_LIST
 
 if [[ -f "_TERRAFORM_DIFF" ]]; then
-  echo -e "Diff: \n${_TERRAFORM_DIFF}"
+  echo -e "Diff: \n$(cat _TERRAFORM_DIFF)"
   reviewdog -f=diff -name="terraform fmt" -reporter=github-pr-review < _TERRAFORM_DIFF
   exit 1
 fi
